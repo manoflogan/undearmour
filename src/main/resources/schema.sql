768fa0862283854de3fecb9c0026e2fc2c3af6ef
Kartik Krishnanand <krishnanandk@gmail.com>
2017-11-06 02:02:52 -0800

    Adding indexes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,1 @@
-DROP TABLE IF EXISTS Chats;CREATE TABLE IF NOT EXISTS Chats(username VARCHAR(20), chat_text VARCHAR(256), chat_id BIGINT, expiration_date TIMESTAMP, PRIMARY KEY(chat_id) )
+DROP TABLE IF EXISTS Chats;CREATE TABLE IF NOT EXISTS Chats(username VARCHAR(20), chat_text VARCHAR(256), chat_id BIGINT, expiration_date TIMESTAMP, PRIMARY KEY(chat_id)); CREATE INDEX IF NOT EXISTS username_index on Chats(username); CREATE INDEX IF NOT EXISTS chat_id_index on Chats(chat_id); CREATE INDEX IF NOT EXISTS expiration_date_index on Chats(expiration_date);
